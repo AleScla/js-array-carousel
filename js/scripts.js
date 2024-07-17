4aeb6a5016b95bfa37052416eaf416aa361686d2
AleScla <scla.alessandro@gmail.com>
2024-07-17 12:25:25 +0200

    strutturato html e css inserito array img

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+console.log('js collegato')
+
+const myImg = [
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp'
+]
+console.log(myImg)
+
+const container = document.querySelector('.container')
+container.append(myImg[1])
