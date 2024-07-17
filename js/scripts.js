e64aab7cbfa908395cbea4ee45bf9aa5d654d3e1
AleScla <scla.alessandro@gmail.com>
2024-07-17 15:02:23 +0200

    esercizio completato con bonus carosello infinito

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,87 @@
-console.log('js collegato')
+console.log('js collegato');
 
-const myImg = [
+let myImg = [
     'img/01.webp',
     'img/02.webp',
     'img/03.webp',
     'img/04.webp',
     'img/05.webp'
 ]
-console.log(myImg)
+console.log(myImg);
 
-const container = document.querySelector('.container')
-container.append(myImg[1])
+const container = document.querySelector('.container');
+
+
+container.innerHTML += `
+<div class="img-container active">
+   <img src="${myImg[0]}">
+</div>
+<div class="img-container">
+   <img src="${myImg[1]}">
+</div>
+<div class="img-container">
+   <img src="${myImg[2]}">
+</div>
+<div class="img-container">
+   <img src="${myImg[3]}">
+</div>
+<div class="img-container">
+   <img src="${myImg[4]}">
+</div>
+
+`   
+
+const imgContainer = document.getElementsByClassName('img-container');
+console.log(imgContainer);
+
+let active = 0;
+
+const btn = document.querySelector('.down');
+btn.addEventListener('click', function(){
+    if(active < myImg.length - 1){
+        imgContainer[active].classList.remove('active');
+
+        active++;
+
+        imgContainer[active].classList.add('active');
+    }  
+    else if (active == myImg.length - 1){
+
+        imgContainer[active].classList.remove('active');
+
+        active = 0;
+
+        imgContainer[active].classList.add('active'); 
+    }  
+    }
+)
+
+const btntop = document.querySelector('.top');
+btntop.addEventListener('click', function(){
+    if(active > 0){
+        imgContainer[active].classList.remove('active');
+
+        active--;
+
+        imgContainer[active].classList.add('active');
+    }  
+    else if (active == 0){
+
+        imgContainer[active].classList.remove('active');
+
+        active = myImg.length - 1;
+
+        imgContainer[active].classList.add('active'); 
+    }  
+});
+    
+
+
+
+
+
+
+    
+
+
+
